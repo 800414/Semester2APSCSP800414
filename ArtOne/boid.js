afe086cb8847ce39d32ee4be8dd999333179c292
800414 <800414@seq.org>
2020-01-29 09:49:30 -0800

    honestly looks cool

--- ArtOne/boid.js
+++ ArtOne/boid.js
@@ -11,10 +11,30 @@ class Boid {
    this.checkEdges();
  }
  update(){
-
+this.vel.limit(3);
+this.vel.add(this.acc);
+this.loc.add(this.vel);
  }
 
  render(){
-
+fill(this.clr);
+ellipse(this.loc.x, this.loc.y, 30, 30);
+fill(147,112,219);
+//for var i = boids.length-1; i>0;
+line(this.loc.x, this.loc.y,100,100)
  }
+ checkEdges(){
+   if(this.loc.x < 0){
+     this.vel.x = -this.vel.x;
+   }
+   if(this.loc.x > width){
+     this.vel.x = -this.vel.x;
+   }
+   if(this.loc.y < 0){
+     this.vel.y = -this.vel.y;
+   }
+   if(this.loc.y > height){
+     this.vel.y = -this.vel.y;
+ }
+}
 }
--- ArtOne/sketch.js
+++ ArtOne/sketch.js
@@ -6,14 +6,25 @@ var boids = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
+  background(255);
   fill(200, 30, 150);
-  loadBoids();
+  loadBoids(10);
 
 
 }
 
 //  The draw function is called @ 30 fps
-function draw() {
+function draw(){
+runBoids();
+}
 
+function loadBoids(n){
+  for(var i = 0; i < n; i++){
+    boids[i] = new Boid(random(100,700),random(100,700), random(-2,2), random(-2,2));
+}
+}
+function runBoids(){
+  for(var i = 0; i < boids.length; i++){
+    boids[i].run();
+  }
 }
