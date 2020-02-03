20376930aaca98686d05ebb4cf847b077fc70eca
800414 <800414@seq.org>
2020-02-03 09:43:38 -0800

    did it brother

--- ArtTwo/boid.js
+++ ArtTwo/boid.js
@@ -1,3 +1,7 @@
+//  Olivia Cordero
+// 	1/30/20
+//  ArtTwo
+//  The setup function function is called once when your program begins
 class Boid {
  constructor(x,y,dx,dy){  //factors of boid, location, velocity, acceleration, color
    this.loc = createVector(x, y);
@@ -17,9 +21,7 @@ this.loc.add(this.vel);  //add velocity to acceleration
  }
 
  render(){
-for (var i = boids.length-1; i>0; i--){  //determine if biods are less than 200 pixels apart
-  //fill(this.clr);
-  //ellipse(this.loc.x,this.loc.y, 59, 72);
+for (var i = boids.length-1; i>0; i--){  //determine if boids are less than 200 pixels apart
   if(this.loc.dist(boids[i].loc)<200){
     stroke(this.clr);  //draw line
     line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
--- ArtTwo/sketch.js
+++ ArtTwo/sketch.js
@@ -1,6 +1,6 @@
 //  Olivia Cordero
-// 	1/28/20
-//  ArtOne
+// 	1/30/20
+//  ArtTwo
 //  The setup function function is called once when your program begins
 var boids = [];  //global variable to make boids
 function setup() {
@@ -14,17 +14,17 @@ function setup() {
 }
 
 //  The draw function is called @ 30 fps
-function draw(){
+function draw(){  //run runBoids function
 runBoids();
 }
 
-function loadBoids(n){
+function loadBoids(n){  //make array of boids appear
   for(var i = 0; i < n; i++){
-    boids[i] = new Boid(400,400, random(-2,2), random(-2,2));
+    boids[i] = new Boid(400,400, random(-2,2), random(-2,2)); //appear at middle of the screen, velocity and acceleration in between -2 and 2
 }
 }
 function runBoids(){
-  for(var i = 0; i < boids.length; i++){
+  for(var i = 0; i < boids.length; i++){ //run boid
     boids[i].run();
   }
 }
