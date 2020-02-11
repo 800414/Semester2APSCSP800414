9d0aeca6d5e0b42e4efe4cb8c12d47bbe41848a7
800414 <800414@seq.org>
2020-02-11 09:48:36 -0800

    think i b done

--- MyArt Project/boid.js
+++ MyArt Project/boid.js
@@ -3,54 +3,56 @@
 //  ArtTwo
 //  The setup function function is called once when your program begins
 class Boid {
- constructor(x,y,dx,dy){  //factors of boid, location, velocity, acceleration, color
-   this.loc = createVector(x, y);
-   this.vel = createVector(dx, dy);
-   this.acc = createVector(3,3);
-   this.clr = color(random(255), random(255), random(255));
- }
- run(){
-   this.update();
-   this.render();
-   this.checkEdges();
- }
- update(){
-this.vel.limit(10);  //setting velocity of boid to ten
-this.vel.add(this.acc);  //add acceleration to velocity
-this.loc.add(this.vel);  //add velocity to acceleration
-if(this.loc.y < 200){
-  this.vel.y = -this.vel.y;
-}
-if(this.loc.y < 600){
-  this.vel.y = -this.vel.y;
-}
- }
+  constructor(x,y,dx,dy){  //factors of boid, location, velocity, acceleration, color
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(3,3);
+    this.clr = color(random(255), random(255), random(255));
+  } // end constructor+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  run(){
+    this.update();
+    this.render();
+    this.checkEdges();
+  } // end run+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  update(){
+    if(toggle > 0){  //update stops running when mouse is clicked
+      this.vel.limit(10);  //setting velocity of boid to ten
+      this.vel.add(this.acc);  //add acceleration to velocity
+      this.loc.add(this.vel);  //add velocity to acceleration
+      if(this.loc.y < 200){
+        this.vel.y = -this.vel.y;
+      }
+      if(this.loc.y < 600){
+        this.vel.y = -this.vel.y;
+      }
+    }
+  } //end update +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
- render(){
-for (var i = boids.length-1; i>0; i--){  //determine if boids are less than 200 pixels apart
-  var d = this.loc.dist(boids[i].loc)
-  if(d<200){
-    stroke(192, 192, 192, 10);  //draw line
-    line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
-  }
-}
- }
- checkEdges(){  //boids bounce off edges of screen
-   if(this.loc.x < 0){
-     this.vel.x = -this.vel.x;
-     this.acc.x = -this.acc.x;
-   }
-   if(this.loc.x > width){
-     this.vel.x = -this.vel.x;
-     this.acc.x = -this.acc.x;
-   }
-   if(this.loc.y < 0){
-     this.vel.y = -this.vel.y;
-     this.acc.y = -this.acc.y;
-   }
-   if(this.loc.y > height){
-     this.vel.y = -this.vel.y;
-     this.acc.y = -this.acc.y;
- }
-}
-}
+  render(){
+    for (var i = boids.length-1; i>0; i--){  //determine if boids are less than 200 pixels apart
+      var d = this.loc.dist(boids[i].loc)
+      if(d<200){
+        stroke(192, 192, 192, 10);  //draw line
+        line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
+      }
+    }
+  } //end of render+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  checkEdges(){  //boids bounce off edges of screen
+    if(this.loc.x < 0){
+      this.vel.x = -this.vel.x;
+      this.acc.x = -this.acc.x;
+    }
+    if(this.loc.x > width){
+      this.vel.x = -this.vel.x;
+      this.acc.x = -this.acc.x;
+    }
+    if(this.loc.y < 0){
+      this.vel.y = -this.vel.y;
+      this.acc.y = -this.acc.y;
+    }
+    if(this.loc.y > height){
+      this.vel.y = -this.vel.y;
+      this.acc.y = -this.acc.y;
+    }
+  } //end checkEdges+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}  //end boid class+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- MyArt Project/sketch.js
+++ MyArt Project/sketch.js
@@ -2,13 +2,14 @@
 //02/05/20
 //MyArtProject
 //The setup  function is called once when your program begins
-var colors;
+var colors;  //defining global variables
 var type;
 var boids = [];
+var toggle = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  loadBoids(40);
+  loadBoids(40);  //how many boids appear in background
   colors = [
     color(204, 102, 255),
     color(204, 153, 255),
@@ -20,44 +21,44 @@ function setup() {
 
 function draw() {
   blendMode(BLEND);
-
-  if(type == 0) {
-    background(0);
+  if(type == 0) {  //different versions of background
+    background(0); //black
     blendMode(SCREEN);
   } else {
-    background(255, 0, 102);
+    background(255, 0, 102);  //pink
     blendMode(EXCLUSION);
   }
   noFill();
-  strokeWeight(20);
-  for(var i = 0; i < 3; i++) {
+  strokeWeight(20);  //width of curve shape
+  for(var i = 0; i < 3; i++) {  //start drawing shape
     stroke(colors[i]);
     beginShape();
-    for(var w = -20; w < width + 20; w += 5) {
+    for(var w = -20; w < width + 20; w += 5) {  //shape waves in and out
       var h = height / 2;
       h += 200 * sin(w * 0.03 + frameCount * 0.07 + i * TWO_PI / 3) * pow(abs(sin(w * 0.001 + frameCount * 0.02)), 5);
       curveVertex(w, h);
     }
     endShape();
   }
-  runBoids();
+  runBoids();  //boids run in background
 }
 
 function mousePressed() {
-  if(type == 0) {
+  toggle = -toggle;  //negative toggle = boids stop moving
+  if(type == 0) {  //mouse pressed code to change background
     type = 1;
   } else {
     type = 0;
   }
 }
 
-function loadBoids(n){
+function loadBoids(n){  // how boids appear on random areas of screen
   for(var i = 0; i < n; i++){
     boids[i] = new Boid(random(100,700),random(100,700), random(-2,2), random(-2,2));
 }
 }
 function runBoids(){
   for(var i = 0; i < boids.length; i++){
-    boids[i].run();
+    boids[i].run();  //run boid array
   }
 }
